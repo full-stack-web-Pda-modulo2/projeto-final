9dd206c5d5c97082d67905d307bdb8f5fbdd4d46
MatheusSA <matheusdesaaguiar@gmail.com>
2024-11-18 11:10:42 -0300

    feat: adicionando as classes user e suggestion

---
+++ script.js
@@ -0,0 +1,43 @@
+let dataBase = [
+
+]
+
+class User {
+    constructor(name, email, password){
+        this.name = name,
+        this.email = email,
+        this.password = password
+    }
+
+    registerUser(){
+        dataBase.push();
+    }
+
+    editUser() {
+        
+    }
+
+    deleteUser() {
+     dataBase.pop();
+    }
+
+    userLogin() {
+
+    }
+}
+
+class Suggestion {
+    constructor(user, content) {
+        this.user = user,
+        this.content = content
+    }
+
+    createSuggestion() {
+        
+    }
+
+    deleteSuggestion() {
+
+    }
+}
+
