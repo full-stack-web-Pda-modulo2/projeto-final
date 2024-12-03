27a68ded024030cbe5ee8c189530e8589bb98837
odilonskt <odilon123c@gmail.com>
2024-12-03 18:48:22 -0300

    🐛 erro de sintaxe:  trocando getID da linha 21 & 27, de estado para state

--- src/view/viaCep.js
+++ src/view/viaCep.js
@@ -1,55 +1,36 @@
 'use strict'
-
-
 const preencherFormulario = (endereco)=>{
     document.getElementById('street-input').value = endereco.logradouro;
     document.getElementById('neighborhood').value = endereco.bairro;
     document.getElementById('city').value = endereco.localidade;
     document.getElementById('state').value = endereco.uf;
 }
-
-
 //função para validar se o valor contém apenas números
 const eNumero = (numero)=>/^[0-9]+$/.test(numero);
-
 //função que valida o CEP, que deve ter 8 díditos e conter apenas números
 const cepValido = (cep)=> cep.length === 8 && eNumero(cep);
-
-
 const pequisarCep = async() =>{
 try {
     let cep= document.getElementById("cep").value;
-
     cep = cep.replace(/\D/g, '');
-
     const url = `http://viacep.com.br/ws/${cep}/json/`;
-
     if(cepValido(cep)){
         const dados = await fetch(url);
-
         const endereco = await dados.json();
-
         if(endereco.hasOwnProperty('erro')){
-
-            document.getElementById('endereco').value = 'CEP não encontrado!';
+            document.getElementById('state').value = 'CEP não encontrado!';
             throw new Error('CEP não encontrado!')
         }else{
             preencherFormulario(endereco);
         }
     }else{
-        document.getElementById('endereco').value = 'CEP incorreto!'
+        document.getElementById('state').value = 'CEP incorreto!'
         throw new Error('CEP incorreto!')
     }
-
     
     
 } catch (error) {
-
-
     console.log(error.message)
 }    
-
-
 };
-
 document.getElementById("cep").addEventListener('focusout', pequisarCep)
