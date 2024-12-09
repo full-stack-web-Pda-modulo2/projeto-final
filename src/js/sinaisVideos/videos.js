dcb65b3f98eb2a0c355d3083acb2bf503e08cc23
jhulyanne <jhulyannedeoliveira@gmail.com>
2024-12-09 20:00:47 -0300

    chore: adicionar textos descritivos para os videos

--- src/js/sinaisVideos/videos.js
+++ src/js/sinaisVideos/videos.js
@@ -24,16 +24,17 @@ const videosDB = [];
 
 videosDB.push(
     ...[ // o spread "quebra" os itens do array, senão o array push guardaria um outro array inteiro ao invés dos valores separadamente
-        new Signal("Alfabeto", "Teste teste", "ARX0sl6apCw&t=1s"),
-        new Signal("Alfabeto detalhado", "Teste teste", "EaE7R1VBBiA"),
-        new Signal("Cumprimentos", "Teste teste", "vaORm9dr8P4"),
-        new Signal("Dias da semana", "Teste teste", "vaORm9dr8P4"),
-        new Signal("Meses do ano", "Teste teste", "wruW9LIOkS8"), 
-        new Signal("Calendário", "Teste teste", "OE_DUiCNpUw"), 
-
-        new Signal("Meses do ano", "Teste teste", "wruW9LIOkS8"), 
-        new Signal("Meses do ano", "Teste teste", "wruW9LIOkS8"), 
-        new Signal("Meses do ano", "Teste teste", "wruW9LIOkS8"), 
+        new Signal("Alfabeto", "Aprenda os sinais correspondentes às letras do alfabeto.", "ARX0sl6apCw&t=1s"),
+        new Signal("Alfabeto detalhado", "Entenda como executar cada sinal do alfabeto com precisão.", "EaE7R1VBBiA"),
+        new Signal("Cumprimentos", "Descubra como se comunicar com saudações básicas em sinais.", "iCG6wTKJkis"),
+        
+        new Signal("Dias da semana", "Aprenda os sinais para os dias da semana.", "vaORm9dr8P4"),
+        new Signal("Meses do ano", "Memorize os sinais dos meses do ano.", "wruW9LIOkS8"),
+        new Signal("Calendário", "Saiba como sinalizar datas e períodos no calendário.", "OE_DUiCNpUw"),
+        
+        new Signal("Verbos", "Aprenda os sinais para verbos comuns e suas variações.", "Tb4Byr4gfRI"),
+        new Signal("Adjetivos", "Descubra como descrever características com sinais.", "OoNbG9EEJ4Q&t=12s"),
+        new Signal("Emoções e sentimentos", "Expresse emoções e sentimentos usando sinais.", "Xn_rDzXuIrw")
     ]
 );
 
